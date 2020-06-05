e3c8c065f694e399276c1973a65b09cc75deddda
Francisco-11 <informatica.vera2@gmail.com>
2020-06-05 10:17:16 -0400

    Seccion de las peticiones http

---
+++ 04-spotiapp/src/app/components/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: [
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  paises: any[] = [];
+
+  constructor( private http: HttpClient) {
+
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+             .subscribe((data: any) => {
+                this.paises = data;
+                console.log(this.paises);
+                
+             });
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
