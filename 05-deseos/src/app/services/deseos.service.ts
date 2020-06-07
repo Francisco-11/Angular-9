c26cc97e5695dd002806e34198d7c8a3855426ad
Francisco-11 <informatica.vera2@gmail.com>
2020-06-06 20:00:05 -0400

    hasta localstorage de la seccion 7

---
+++ 05-deseos/src/app/services/deseos.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService {
+
+  // Aqui se manejaran las listas de las tareas
+  listas: Lista[] = [];
+
+  constructor() {
+    this.cargarStorage();
+    
+  /*   const lista1 = new Lista('Recolectar las piedras del infinito');
+    const lista2 = new Lista('Héroes a desaparecer');
+
+    this.listas.push(lista1, lista2); */
+    
+
+  }
+
+  crearLista( titulo: string){
+    const nuevaLista =  new Lista(titulo);
+    this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage(){
+    localStorage.setItem('data', JSON.stringify(this.listas ));
+  }
+
+  cargarStorage(){
+    if(localStorage.getItem('data')){
+      this.listas = JSON.parse( localStorage.getItem('data'));
+    }else{
+      this.listas = [];
+    }
+  }
+ 
+}
