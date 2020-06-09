1fb7c3776c49d14da776a1aaed7fbac6f63b99b0
Francisco-11 <informatica.vera2@gmail.com>
2020-06-09 13:05:58 -0400

    uso de rutas hijas y paso de parametros

---
+++ 06-miscelaneos/src/app/components/usuario/usuario.routes.ts
@@ -0,0 +1,17 @@
+// Controlara todas las rutas referente al usuario
+
+import { Routes } from '@angular/router';
+
+import { UsuarioNuevoComponent } from './usuario-nuevo.component';
+import { UsuarioEditarComponent } from './usuario-editar.component';
+import { UsuarioDetalleComponent } from './usuario-detalle.component';
+
+
+
+export const USUARIOS_ROUTES: Routes = [
+    { path: 'nuevo/:paramentro2', component: UsuarioNuevoComponent},
+    { path: 'editar', component: UsuarioEditarComponent},
+    { path: 'detalle', component: UsuarioDetalleComponent},
+    { path: '**', pathMatch: 'full', redirectTo: 'nuevo' },
+
+];
